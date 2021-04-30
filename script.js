4d4581054906ecb7f6a41cd2ef54872425ee6395
Kirill1962 <82878512+Kirill1962@users.noreply.github.com>
2021-04-30 05:10:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var str1 = prompt('Введите число:');
+var min = 60;
+var hour = 3600;
+var sut = 86400;
+console.log((str1 / min) + (' минут'));
+console.log((str1 / hour) + (' часов'));
+console.log((str1 / sut) + (' в сутках'));
